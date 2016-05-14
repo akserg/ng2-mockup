fef5dae01ed6b1841a186fa3760c853610b4100e
Sergey Akopkokhyants <akserg@gmail.com>
2016-05-14 18:52:55 +0200

    Added Sidebar. Updated styles

--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -1,8 +1,10 @@
 import {Component, ElementRef} from '@angular/core';
 
+import {SidebarComponent} from '../sidebar/sidebar.component';
+
 @Component({
   selector: 'header',
-  templateUrl: 'app/header/header.component.html'/*,
-  directives: [Sidebar, HeaderNotification]*/
+  templateUrl: 'app/header/header.component.html',
+  directives: [SidebarComponent] //, HeaderNotification]
 })
 export class HeaderComponent {}
---
+++ app/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import {Component, ElementRef} from '@angular/core';
+
+@Component({
+  selector: 'sidebar',
+  styleUrls: ['app/sidebar/sidebar.component.css'],
+  templateUrl: 'app/sidebar/sidebar.component.html'
+})
+export class SidebarComponent {}
