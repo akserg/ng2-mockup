805d4e7939a0cd23f78330d24d90da0b460f2eea
Sergey Akopkokhyants <akserg@gmail.com>
2016-05-15 13:51:40 +0200

    Added tools buttons

--- app/sidebar/sidebar.component.ts
+++ app/sidebar/sidebar.component.ts
@@ -1,8 +1,11 @@
 import {Component, ElementRef} from '@angular/core';
 
+import {ToolListComponent} from '../tools/tool-list/tool-list.component';
+
 @Component({
   selector: 'sidebar',
   styleUrls: ['app/sidebar/sidebar.component.css'],
-  templateUrl: 'app/sidebar/sidebar.component.html'
+  templateUrl: 'app/sidebar/sidebar.component.html',
+  directives: [ToolListComponent]
 })
 export class SidebarComponent {}
--- app/tools/tool-list/tool-list.component.ts
+++ app/tools/tool-list/tool-list.component.ts
@@ -2,7 +2,7 @@ import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'tool-list',
-  styleUrls: ['app/tool-list/tool-list.component.css'],
-  templateUrl: 'app/tool-list/tool-list.component.html'
+  styleUrls: ['app/tools/tool-list/tool-list.component.css'],
+  templateUrl: 'app/tools/tool-list/tool-list.component.html'
 })
 export class ToolListComponent {}
